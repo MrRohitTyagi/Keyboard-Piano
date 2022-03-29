2c546f822693fde3290fef5e746fa1e8dad3fe0b
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-03-29 22:38:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['a', 's', 'j', 'k', 'l']
+const ARROW_Keys = ['ArrowLeft','ArrowRight','ArrowUp','ArrowDown'] 
 let count = 0;
 
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
+const ArrowKeys = document.querySelectorAll('.cord')
 
 keys.forEach(key => {
   
@@ -16,9 +18,11 @@ document.addEventListener('keydown', e => {
   const key = e.key
   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
   const blackKeyIndex = BLACK_KEYS.indexOf(key)
+  const ArrowKeyIndex = ARROW_Keys.indexOf(key)
 
   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+  if (ArrowKeyIndex > -1) playNote(ArrowKeys[ArrowKeyIndex])
 })
 
   function playNote(key) {
@@ -28,19 +32,30 @@ document.addEventListener('keydown', e => {
   
 
   key.classList.add('active')
+  ArrowKeys.classList.add('active')
   noteAudio.addEventListener('ended', () => {
     key.classList.remove('active')
+    ArrowKeys.classList.remove('active')
   })
-
-  
-
-  
-
-  
-    
-    
-  
-  
-     
   
 }
+let checkboxflag = false
+//guitar
+let AddButton = document.querySelector('#AB')
+let RemoveButton = document.querySelector('#RB')
+
+let guitardisplay = document.querySelector('.full-guitar')
+
+AddButton.addEventListener('click',function(e){
+
+  checkboxflag = !checkboxflag ;
+  guitardisplay.style.display = 'flex'
+  
+})
+RemoveButton.addEventListener('click',function(e){
+
+  checkboxflag = !checkboxflag ;
+  guitardisplay.style.display = 'none'
+  
+})
+
